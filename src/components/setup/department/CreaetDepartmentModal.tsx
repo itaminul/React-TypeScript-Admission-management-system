34423529687b7d1e4730c9ca61961adc8f926569
aminul@atilimited.net <aminul@atilimited.net>
2024-03-18 16:05:53 +0600

    create dept

--- src/components/setup/department/CreaetDepartmentModal.tsx
+++ src/components/setup/department/CreaetDepartmentModal.tsx
@@ -1,5 +1,6 @@
-import { Button, Form, Input, Modal, Select } from "antd";
+import { Button, Form, Input, Modal, Select, message } from "antd";
 import { DepartmentDataType } from "./DepartmentDataType";
+import { useCreateDepartmentSetupMutation } from "../../../redux/features/service/departmentApiService";
 const { Option } = Select;
 interface CreateModalProps {
   open: boolean;
@@ -7,9 +8,28 @@ interface CreateModalProps {
 }
 
 function CreatDepartmentModal({ open, onClose }: CreateModalProps) {
-
-  const onFinish = (value: DepartmentDataType) => {
-    console.log("department value", value);
+const [ createDepartment ]  = useCreateDepartmentSetupMutation();
+  const [form] = Form.useForm();
+  const onFinish = async(value: DepartmentDataType) => {
+    try {
+      const departmentFormat = {
+        departmentName: value.departmentName,
+        departmentDes: value.departmentName,
+        orgId: Number(value.orgId)
+      }
+      console.log("department value", departmentFormat);
+      const response = await createDepartment(departmentFormat);
+      if(response != null) {
+        setTimeout(() =>{
+          void message.success('Created successfully');
+          onClose();
+          window.location.reload();
+        }, 200)
+      }
+      form.resetFields();
+    } catch (error) {
+      console.error("Error create data", error);
+    }
   }
   return (
     <>
@@ -19,19 +39,24 @@ function CreatDepartmentModal({ open, onClose }: CreateModalProps) {
         footer={[
           <>
             <Button onClick={onClose}>Cancel</Button>
-            <Button key="submit"  type="primary" htmlType="submit">
-              Submit
-            </Button>
           </>,
         ]}
       >
-        <Form layout="vertical" onFinish={onFinish}>
+        <Form layout="vertical" onFinish={onFinish} form={form}>
           <Form.Item
             label="Name"
             name="departmentName"
-            rules={[{ required: true }]}
+            rules={[{ required: true,  message: "Please enter department name!"  }]}
           >
             <Input placeholder="Department Name" />
+          </Form.Item>
+
+          <Form.Item
+            label="Department Description"
+            name="departmentDes"
+            rules={[{ required: false }]}
+          >
+            <Input placeholder="Department Descriptoin" />
           </Form.Item>
 
           <Form.Item
@@ -43,6 +68,16 @@ function CreatDepartmentModal({ open, onClose }: CreateModalProps) {
               <Option value="1">Abc</Option>
               <Option value="2">Def</Option>
             </Select>
+          </Form.Item>
+           <Form.Item>
+            <Button
+              style={{ float: 'left' }}
+              key="submit"
+              type="primary"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
           </Form.Item>
         </Form>
       </Modal>
